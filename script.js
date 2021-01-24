5b1def4b9f33176e2db43963d902c7100c8529dd
arzzcom <arzz@arzz.com>
2021-01-24 19:39:25 +0900

    버전 통합

--- script.js
+++ script.js
@@ -7,38 +7,23 @@
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
  * @version 1.0.0
- * @modified 2020. 12. 28.
+ * @modified 2021. 1. 21.
  */
-if (Minitalk.version < 70000) {
-	/**
-	 * 미니톡 v7.0 이하의 버전에서 안티스팸 플러그인 적용을 받지 않을 최소권한
-	 * ALL : 전체유저가 안티스팸 플러그인 적용을 받지 않음
-	 * MEMBER : 회원권한 이상인 경우 안티스팸 플러그인 적용을 받지 않음
-	 * ADMIN : 관리자권한 이상인 경우 안티스팸 플러그인 적용을 받지 않음
-	 */
-	me.limit = "MEMBER";
-} else {
-	/**
-	 * 미니톡 v7.0 이상의 버전에서 안티스팸 플러그인 적용을 받지 않을 최소권한
-	 * 0 ~ 9 : 0 : 손님, 9 : 관리자
-	 */
-	me.limit = 1;
-}
+if (Minitalk === undefined) return;
+
+/**
+ * 안티스팸 플러그인 적용을 받지 않을 최소권한
+ * 0 ~ 9 : 0 : 손님, 9 : 관리자
+ */
+me.limit = 0;
 
 // 메시지 전송직전 URL 이 포함되어 있는지 확인한다.
 Minitalk.on("beforeSendMessage",function(minitalk,message,user) {
 	// 메시지에 URL 이 포함된 경우
 	if (message.search(/(http|https|ftp):\/\//) >= 0) {
-		if (Minitalk.version < 70000) {
-			if (Minitalk.user.checkLimit(me.limit,user.opper) == true) {
-				minitalk.ui.printSystemMessage("error","채팅메시지에 URL을 포함할 수 없습니다.");
-				return false;
-			}
-		} else {
-			if (user.level < me.limit) {
-				minitalk.ui.printSystemMessage("error","채팅메시지에 URL을 포함할 수 없습니다.");
-				return false;
-			}
+		if (user.level < me.limit) {
+			minitalk.ui.printSystemMessage("error","채팅메시지에 URL을 포함할 수 없습니다.");
+			return false;
 		}
 	}
 });
